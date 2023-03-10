1e8fcb9a27ddec5eeebae32f8b846e0c515dffd6
luvaldev <luisvmondaca@gmail.com>
2023-03-10 16:38:24 -0300

    Subida carpeta

---
+++ Clase 1.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+main()
+{
+	cout << "Bienvenidos a programacion"<<endl; 
+	cout << "Hola"<< endl << "Mucho gusto" << endl << "¿Todo bien?"<< endl;
+	cout << "----------- Numeros ----------"<< endl;
+	cout << 10/3 << endl;
+	cout << 1/3 << endl;
+	cout << 24/9 << endl;
+	cout << 10.0/3 << endl;
+	cout << 1.0/3 << endl;
+	cout << 24 % 9 << endl;
+	cout << 12345 % 10 << endl;
+	cout << 12345 % 100 << endl;
+	cout << 12345 % 1000 << endl;	
+}
+
+//Linea 6 de texto para mostrar una linea de texto
+//Linea 7 para mostrar diferentes lineas de texto
+//Linea 9/10/11 para realizar un calculo matematico sin decimales y se aproxima
+//Linea 12/13 para realizar un calculo matematico con decimales y exacto
+//Linea 14 simbolo "%" (modulo) da el resto de la divicion de la Linea 11
+//Linea 15/16/17 Sus modulos "%" al ser numeros provenientes del 10 se cuenta en la cantidad de 0 para tomar los ultimos digitos
+
+
+//Tipos de datos/datos primitivos (string-int-float-booleand-char)
+//Bucle (for-while)
+//
+		
+
