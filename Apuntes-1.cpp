fbc8f7a8da45e2e9aeba5bf79213f94ef3d5a2fe
luvaldev <luisvmondaca@gmail.com>
2023-03-10 21:46:06 -0300

    Apuntes

--- Apuntes-1.cpp
+++ Apuntes-1.cpp
@@ -1,29 +1,25 @@
 #include <iostream> //Libreria Input Output Stream (Entrada y Salida de Datos y Texto)
 using namespace std;
 
-main() //Funcion principal
+main() // Funcion principal
 {
 	cout << "Apuntes 1" << endl;
 	cout << " " << endl;
 	cout << "Hola" << endl;
 	cout << " " << endl;
-	//cin.get();
+	// cin.get();
 
 	cout << "Variables" << endl;
 	cout << " " << endl;
 
-	int entero = 10; //Variable numerica
+	int entero = 10; // Variable numerica
 	int Si = 15;
 
 	cout << entero;
 	cout << " " << endl;
-	cout << Si + entero; //Suma de variables establecidas se puede hacer con cualquier operacion
-
-
-
+	cout << Si + entero; // Suma de variables establecidas se puede hacer con cualquier operacion
 
 	return 0;
-
 }
 
 // cout para mostrar texto en pantalla
