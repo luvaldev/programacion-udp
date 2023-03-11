46423cfc67c3e2a66ce2e597bc3e276fca4b03a6
Matías Castañeda Mondaca <matiascmondaca@gmail.com>
2023-03-10 21:13:09 -0300

    Subida de apuntes y perfil vscode

---
+++ Apuntes-1.cpp
@@ -0,0 +1,31 @@
+#include <iostream> //Libreria Input Output Stream (Entrada y Salida de Datos y Texto)
+using namespace std;
+
+main() //Funcion principal
+{
+	cout << "Apuntes 1" << endl;
+	cout << " " << endl;
+	cout << "Hola" << endl;
+	cout << " " << endl;
+	//cin.get();
+
+	cout << "Variables" << endl;
+	cout << " " << endl;
+
+	int entero = 10; //Variable numerica
+	int Si = 15;
+
+	cout << entero;
+	cout << " " << endl;
+	cout << Si + entero; //Suma de variables establecidas se puede hacer con cualquier operacion
+
+
+
+
+	return 0;
+
+}
+
+// cout para mostrar texto en pantalla
+// cin para que el usuario escriba un texto para finalizar
+// return 0; para finalizar el programa
